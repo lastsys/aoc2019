6738df98fb366c8faca0aba1d2ce9cf4258d418f
Stefan Larsson <stefan@lastsys.com>
2019-12-16 08:01:19 +0100

    Added day 12 part 2.

--- src/main/scala/com/lastsys/aoc2019/day12/Day12.scala
+++ src/main/scala/com/lastsys/aoc2019/day12/Day12.scala
@@ -1,6 +1,9 @@
 package com.lastsys.aoc2019.day12
 
 import com.lastsys.aoc2019.util.{AocTask, loadResource}
+
+import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.util.chaining._
 
 case class Vector3(x: Int, y: Int, z: Int) {
@@ -35,6 +38,10 @@ object Day12 extends AocTask {
       }
       val tot = step1000.map(_.tot).sum
       println(s"Day12 :: Part1 = $tot")
+
+      val r = findCycles(moons)
+      val f = lcm(r._1, lcm(r._2, r._3))
+      println(s"Day12 :: Part2 = $f")
     }
   }
 
@@ -73,4 +80,45 @@ object Day12 extends AocTask {
   def applyVelocity(moons: Seq[Moon]): Seq[Moon] = {
     moons.map { moon => moon.copy(p = moon.p + moon.v) }
   }
+
+  def findCycles(moons: Seq[Moon]): (Int, Int, Int) = {
+    // Coordinates are independent.
+    val xSet: mutable.Set[Set[(Int, Int)]] = mutable.Set.empty
+    val ySet: mutable.Set[Set[(Int, Int)]] = mutable.Set.empty
+    val zSet: mutable.Set[Set[(Int, Int)]] = mutable.Set.empty
+    var xCount = 0
+    var yCount = 0
+    var zCount = 0
+    var step = moons
+    var count = 0
+    while (!(xCount > 0 && yCount > 0 && zCount > 0)) {
+      val xState = step.foldLeft(Set.empty[(Int, Int)]) { (acc, m) =>
+        acc + ((m.p.x, m.v.x))
+      }
+      val yState = step.foldLeft(Set.empty[(Int, Int)]) { (acc, m) =>
+        acc + ((m.p.y, m.v.y))
+      }
+      val zState = step.foldLeft(Set.empty[(Int, Int)]) { (acc, m) =>
+        acc + ((m.p.z, m.v.z))
+      }
+
+      if (xCount == 0 && xSet.contains(xState)) xCount = count
+      if (yCount == 0 && ySet.contains(yState)) yCount = count
+      if (zCount == 0 && zSet.contains(zState)) zCount = count
+
+      xSet += xState
+      ySet += yState
+      zSet += zState
+
+      step = step.pipe(applyGravity).pipe(applyVelocity)
+
+      count += 1
+    }
+    (xCount, yCount, zCount)
+  }
+
+  @tailrec
+  def gcd(x: Long, y: Long): Long = if (y == 0) Math.abs(x) else gcd(y, x % y)
+
+  def lcm(x: Long, y: Long) = x * y / gcd(x, y)
 }
